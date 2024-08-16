a65ec7dbbd2d72928e8b382c20c240c43f11ce91
Thibault <thibault.barral@epsi.fr>
2024-08-16 15:50:13 +0200

    maj mots

--- src/app/words/page.js
+++ src/app/words/page.js
@@ -19,12 +19,12 @@ export default function WordsList() {
         <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center p-4">
             <h1 className="text-2xl font-bold mb-4">Liste des Mots</h1>
             <table className="table-auto bg-white shadow-md rounded-lg">
-                <thead>
+                {/* <thead>
                     <tr>
                         <th className="px-4 py-2">Mot Commun</th>
                         <th className="px-4 py-2">Mot Undercover</th>
                     </tr>
-                </thead>
+                </thead> */}
                 <tbody>
                     {words.map(([commonWord, undercoverWord], index) => (
                         <tr key={index}>
